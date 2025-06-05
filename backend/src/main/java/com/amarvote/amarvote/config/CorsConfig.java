2faa64214b9ce3e5198a7cc0954234d416082502
Masnoon Muztahid <2005067@ugrad.cse.buet.ac.bd>
2025-06-05 18:57:03 +0600

    omitted some unnecessary things and updated the security config

--- backend/src/main/java/com/amarvote/amarvote/config/CorsConfig.java
+++ backend/src/main/java/com/amarvote/amarvote/config/CorsConfig.java
@@ -1,14 +1,12 @@
 package com.amarvote.amarvote.config;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
@@ -28,15 +26,4 @@ public class CorsConfig {
         return source;
     }
     
-    // @Bean
-    // public CorsFilter corsFilter() {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowCredentials(true);
-    //     config.setAllowedOrigins(Collections.singletonList("http://localhost:5173")); // Frontend URL
-    //     config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization"));
-    //     config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
-    //     source.registerCorsConfiguration("/**", config);
-    //     return new CorsFilter(source);
-    // }
 }
