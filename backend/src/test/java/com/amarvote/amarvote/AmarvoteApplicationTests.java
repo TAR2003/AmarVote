85545e6a7f879094b65f53f99b0036e02c3bc690
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2025-07-24 20:24:52 +0600

    add context in backend test

--- backend/src/test/java/com/amarvote/amarvote/AmarvoteApplicationTests.java
+++ backend/src/test/java/com/amarvote/amarvote/AmarvoteApplicationTests.java
@@ -1,8 +1,13 @@
 package com.amarvote.amarvote;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AmarvoteApplicationTests {
-	// No tests present. Central test class only.
+
+	@Test
+	void contextLoads() {
+	}
+
 }
