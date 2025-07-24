55862741818ea02587107baaa1b7147e41a279ee
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2025-07-24 17:08:24 +0600

    fix the error test

--- backend/src/test/java/com/amarvote/amarvote/AmarvoteApplicationTests.java
+++ backend/src/test/java/com/amarvote/amarvote/AmarvoteApplicationTests.java
@@ -1,13 +1,8 @@
 package com.amarvote.amarvote;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AmarvoteApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+	// No tests present. Central test class only.
 }
