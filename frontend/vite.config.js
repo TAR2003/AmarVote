f388b62badb686632bc21028fe26b117c9c7120c
TAR2003 <108563700+TAR2003@users.noreply.github.com>
2025-11-16 17:23:11 +0600

    made http again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./certs/localhost.key")),
-      cert: fs.readFileSync(path.resolve(path.dirname(fileURLToPath(import.meta.url)), "./certs/localhost.crt")),
-    },
     host: "0.0.0.0",
     proxy: {
       "/api": {
